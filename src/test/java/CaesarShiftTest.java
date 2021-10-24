0470faf4d4a532f549219e716ff537fa0fa5eb12
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-10-25 01:58:18 +0300

    add case test method

--- src/test/java/CaesarShiftTest.java
+++ src/test/java/CaesarShiftTest.java
@@ -4,7 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CaesarShiftTest {
     @Test
+    public void caesarShift_convertCase_String() {
+        CaesarShift testCase = new CaesarShift();
+        assertEquals("A", testCase.encrypt("a"));
+    }
 
+    @Test
     public void caesarShift_encryptChar_String(){
         CaesarShift testType = new CaesarShift();
         assertEquals("Y", testType.encrypt("Z"));
