a7d67e730029918f40f6dd73276d8052c4a0e9cb
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-10-25 10:22:26 +0300

    add string encryption method

--- src/main/java/CaesarShift.java
+++ src/main/java/CaesarShift.java
@@ -1,7 +1,23 @@
+import java.util.ArrayList;
+
 public class CaesarShift {
+    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //Encapsulate and make the alphabet immutable
+    private static int key = 25; //Initialize and encapsulate the shift key
+    private static String encryptedMsg; //Create and encapsulate the message container
 
     public String encrypt(String msg){
-      return msg.toUpperCase();
+        String upCased = msg.toUpperCase();
+        char[] upCasedArrs = upCased.toCharArray();//split the string to a character array
+        ArrayList<Character> encryptedChars = new ArrayList<Character>();
+
+        //loop through the character array
+        for(Character character : upCasedArrs ){
+            int index = ALPHABET.indexOf(character.toString());//get the character rank in the alphabet
+            int encryptedCharIndex = (index+key)%26;//shift the character using the key and get the new characters rank in the alphabet
+            encryptedChars.add(ALPHABET.charAt(encryptedCharIndex));//get the character from the alphabet rank and add it to the char array
+            encryptedMsg = encryptedChars.toString().replaceAll("\\[|\\]|\\s","").replaceAll(",","");//convert and cleanup the char array to a string
+        }
+      return encryptedMsg;
     }
 
 }
