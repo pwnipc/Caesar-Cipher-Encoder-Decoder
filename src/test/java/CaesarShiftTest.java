502d8aeed65dde120905124035ea5116fa9baafe
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-10-25 08:50:50 +0300

    add stringEncrypt test method

--- src/test/java/CaesarShiftTest.java
+++ src/test/java/CaesarShiftTest.java
@@ -6,13 +6,18 @@ class CaesarShiftTest {
     @Test
     public void caesarShift_convertCase_String() {
         CaesarShift testCase = new CaesarShift();
-        assertEquals("A", testCase.encrypt("a"));
+        assertEquals("Z", testCase.encrypt("a"));
     }
 
     @Test
-    public void caesarShift_encryptChar_String(){
-        CaesarShift testType = new CaesarShift();
-        assertEquals("Y", testType.encrypt("Z"));
+    public void caesarShift_encryptSingleChar_String(){
+        CaesarShift encryptChar = new CaesarShift();
+        assertEquals("Y", encryptChar.encrypt("Z"));
     }
 
+    @Test
+    void caesarShift_encryptString_String() {
+        CaesarShift encryptString = new CaesarShift();
+        assertEquals("GDKKN", encryptString.encrypt("hello"));
+    }
 }
