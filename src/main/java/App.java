49ba4052b461546ea8e57d0d472857d56f4a55d8
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-10-25 14:40:56 +0300

    instantiate and implement the encode, decode methods

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,13 @@
+import java.io.Console;
+
 public class App {
     public static void main(String[] args){
-        System.out.println("Sanity Check");
+        Console myConsole = System.console();
+        System.out.println("Enter the text to Encrypt: ");
+        String input = myConsole.readLine();
+        CaesarShift encDec = new CaesarShift();
+        System.out.println("Input String: " + input);
+        System.out.println("Encrypted: " + encDec.encrypt(input));
+        System.out.println("Decrypted String: "+encDec.decrypt(encDec.encrypt(input)));
     }
 }
